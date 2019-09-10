963fae63cc668c380fa0481a59129d8c74552196
Papu <diegortizte@gmail.com>
2019-09-10 12:01:38 -0400

    Initial Commit

---
+++ Game Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game Project
+//
+//  Created by Andres J Fuster on 9/10/19.
+//  Copyright © 2019 Anonymous. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
